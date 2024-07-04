1f414d58070acfd853adf8f0b1e99c9051ca7d97
evymari <144184061+evymari@users.noreply.github.com>
2024-07-04 18:31:11 +0200

    creation main

---
+++ src/components/mainBotton/MainBotton.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './mainBotton.css'
+
+const MainBotton = () => {
+    return (
+        <>
+         <section className='container-news'>
+            <div>
+            <img src={'./src/assets//photo-1.png'} />
+            </div>
+            <div>
+            <img src={'./src/assets/rhinoceros-.png'} />
+            </div>
+            <div>
+            <img src={'./src/assets/photo-2.png'} />
+            </div>
+        </section>
+        </>
+      )
+}
+
+export default MainBotton
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,17 @@
+import NavBar from "../components/navbar/Navbar"
+import Footer from "../components/footer/Footer"
+import MainHomeTop from "../components/mainHome/MainHomeTop"
+import MainBotton from "../components/mainBotton/MainBotton"
  const Home = () => {
     return (
-      <div>Home home</div>
-    )
+      <>
+      
+      <NavBar></NavBar>
+      <MainHomeTop></MainHomeTop>
+      <MainBotton></MainBotton>
+      <Footer></Footer>
+      </>
+      )
   }
   
   export default Home 
